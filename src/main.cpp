e14ee5cf39277f147eb036f62d60e92a93cc5171
Moritz Bösenberg <moritz.boesenberg@outlook.com>
2021-04-13 18:48:18 +0200

    added energy saving stuff and current weather

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@
 
 RTC_DATA_ATTR int bootCount = 0;
 
-StaticJsonDocument<5000> doc;
+StaticJsonDocument<5000> docWeatherNow;
 StaticJsonDocument<5000> docWeather;
 
 String month_wifi;
@@ -64,58 +64,73 @@ GxEPD_Class display(io, EPD_RSET, EPD_BUSY);
 
 
 // Define NTP Client to get time
-HTTPClient http;
+HTTPClient httpWeatherNow;
 HTTPClient httpWeather;
 
 void set_WiFi()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin("FRITZ!Box 7490", "58625300401074548598");
-    // WiFi.scanNetworks will return the number of networks found
+  // WiFi.scanNetworks will return the number of networks found
 
-  delay(500);
+  int count = 0;
   while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
-    Serial.println("Connection Failed!");
+/*     Serial.println("Connection Failed!");
+ */
     delay(1000);
+
+    count++;
+
+    if(count == 10){
+      esp_deep_sleep_start();
+    }
+   
   }
 
-  Serial.println("Ready");
+/*   Serial.println("Ready");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP()); */
 }
 
 void get_weather(){
 
 
   httpWeather.begin("http://api.openweathermap.org/data/2.5/forecast?q=Freiburg,de&cnt=3&units=metric&appid=03e2fbe874af4836c6bf932b697a809b");
+  httpWeatherNow.begin("http://api.openweathermap.org/data/2.5/weather?q=Freiburg,de&cnt=3&units=metric&appid=03e2fbe874af4836c6bf932b697a809b");
   int httpCodeWeather = httpWeather.GET();
+  int httpCodeWeatherNow = httpWeatherNow.GET();
 
-  if (httpCodeWeather > 0)
+  if (httpCodeWeather > 0 || httpCodeWeatherNow > 0)
   { //Check for the returning code
 
     DeserializationError errorWeather = deserializeJson(docWeather, httpWeather.getString());
+    DeserializationError errorWeatherNow = deserializeJson(docWeatherNow, httpWeatherNow.getString());
 
-    if (errorWeather)
+    if (errorWeather || errorWeatherNow)
     {
-      Serial.print(F("deserializeJson() from weather failed: "));
-      Serial.println(errorWeather.c_str());
-      return;
-      delay(2000); //2sec
+/*       Serial.print(F("deserializeJson() from weather failed: "));
+      Serial.println(errorWeather.c_str()); */
+      esp_deep_sleep_start();
+            
     }
     else
     {
-      main_temp_now = docWeather["list"][0]["main"]["temp"];         //Get current time forecast 0h
-      feels_like = docWeather["list"][0]["main"]["feels_like"];
-      String w_d = docWeather["list"][0]["weather"][0]["main"];
-      
-      weather_description_now = w_d;
 
+      display.init();
+
+      display.fillScreen(GxEPD_WHITE);
+
+      main_temp_now = docWeatherNow["main"]["temp"];         //Get current time forecast 0h
+      feels_like = docWeatherNow["main"]["feels_like"];
+      
       main_temp_03 = docWeather["list"][1]["main"]["temp"];         //Get current time forecast 0h
       weather_description_03 = docWeather["list"][1]["weather"][0]["id"];
+      String weather_icon_03 = docWeather["list"][1]["weather"][0]["icon"];
 
       main_temp_06 = docWeather["list"][2]["main"]["temp"];         //Get current time forecast 0h
       weather_description_06 = docWeather["list"][2]["weather"][0]["id"];
+      String weather_icon_06 = docWeather["list"][2]["weather"][0]["icon"];
 
       String dt_txt_now = docWeather["list"][0]["dt_txt"];
       month_wifi =  dt_txt_now.substring(5,7);
@@ -131,59 +146,82 @@ void get_weather(){
       String c = docWeather["city"]["name"];
       city = c;
 
-    Serial.println(weather_description_06);
+    if ((weather_description_03 >=200) && (weather_description_03 <= 232)){
+      display.drawBitmap(thunderstorm_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+    }else if((weather_description_03 >=300) && (weather_description_03 <= 321)){
+      display.drawBitmap(shower_rain_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+    }else if((weather_description_03 >=500) && (weather_description_03 <= 504)){
+      display.drawBitmap(rain_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+    }else if((weather_description_03 >=520) && (weather_description_03 <= 531)){
+      display.drawBitmap(shower_rain_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+    }else if(weather_description_03 == 511){
+      display.drawBitmap(snow_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+    }else if((weather_description_03 >=600) && (weather_description_03 <= 622)){
+      display.drawBitmap(snow_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+    }else if((weather_description_03 >=701) && (weather_description_03 <= 781)){
+      display.drawBitmap(mist_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+    }else if(weather_description_03 == 800 ){
+      if(weather_icon_03.substring(2,3) == "d"){
+        display.drawBitmap(clear_sky_small, 35, 89, 65, 65, GxEPD_WHITE);
+
+      }else{
+        display.drawBitmap(clear_sky_night_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+      }
+    }else if((weather_description_03 >=801) && (weather_description_03 <= 804)){
+      if(weather_description_03 == 801){
+        display.drawBitmap(few_clouds_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+      }else if (weather_description_03 == 802)
+      {
+        display.drawBitmap(scattered_clouds_small, 15, 79, 100, 100, GxEPD_WHITE);
+
+      }else
+      {
+        display.drawBitmap(broken_clouds_small, 15, 81, 100, 100, GxEPD_WHITE);
+      }
+    }
 
     if ((weather_description_06 >=200) && (weather_description_06 <= 232)){
       display.drawBitmap(thunderstorm_small, 15, 167, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
     }else if((weather_description_06 >=300) && (weather_description_06 <= 321)){
       display.drawBitmap(shower_rain_small, 15, 167, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
-    }else if((weather_description_06 >=500) && (weather_description_06 <= 531)){
+    }else if((weather_description_06 >=500) && (weather_description_06 <= 504)){
       display.drawBitmap(rain_small, 15, 167, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
+    }else if((weather_description_06 >=520) && (weather_description_06 <= 531)){
+      display.drawBitmap(shower_rain_small, 15, 167, 100, 100, GxEPD_WHITE);
+    }else if(weather_description_06 == 511){
+      display.drawBitmap(snow_small, 15, 167, 100, 100, GxEPD_WHITE);
     }else if((weather_description_06 >=600) && (weather_description_06 <= 622)){
       display.drawBitmap(snow_small, 15, 167, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
     }else if((weather_description_06 >=701) && (weather_description_06 <= 781)){
       display.drawBitmap(mist_small, 15, 167, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
     }else if(weather_description_06 == 800 ){
-      display.drawBitmap(clear_sky_small, 15, 167, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
+      if(weather_icon_06.substring(2,3) == "d"){
+        display.drawBitmap(clear_sky_small, 35, 177, 65, 65, GxEPD_WHITE);
+      }else{
+        display.drawBitmap(clear_sky_night_small, 15, 167, 100, 100, GxEPD_WHITE);
+      }
     }else if((weather_description_06 >=801) && (weather_description_06 <= 804)){
-      //few_clouds_small
-      //broken_clouds_small
-      display.drawBitmap(scattered_clouds_small, 15, 167, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
+      if(weather_description_06 == 801){
+        display.drawBitmap(few_clouds_small, 15, 167, 100, 100, GxEPD_WHITE);
+      }else if (weather_description_06 == 802)
+      {
+        display.drawBitmap(scattered_clouds_small, 15, 167, 100, 100, GxEPD_WHITE);
+      }else
+      {
+        display.drawBitmap(broken_clouds_small, 15, 167, 100, 100, GxEPD_WHITE);
+      }
     }
-
-    if ((weather_description_03 >=200) && (weather_description_03 <= 232)){
-      display.drawBitmap(thunderstorm_small, 15, 85, 100, 100, GxEPD_WHITE);
+      
       display.setTextColor(GxEPD_BLACK);
-    }else if((weather_description_03 >=300) && (weather_description_03 <= 321)){
-      display.drawBitmap(shower_rain_small, 15, 85, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
-    }else if((weather_description_03 >=500) && (weather_description_03 <= 531)){
-      display.drawBitmap(rain_small, 15, 85, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
-    }else if((weather_description_03 >=600) && (weather_description_03 <= 622)){
-      display.drawBitmap(snow_small, 15, 85, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
-    }else if((weather_description_03 >=701) && (weather_description_03 <= 781)){
-      display.drawBitmap(mist_small, 15, 85, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
-    }else if(weather_description_03 == 800 ){
-      display.drawBitmap(clear_sky_small, 15, 85, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
-    }else if((weather_description_03 >=801) && (weather_description_03 <= 804)){
-      //few_clouds_small
-      //broken_clouds_small
-      display.drawBitmap(scattered_clouds_small, 15, 85, 100, 100, GxEPD_WHITE);
-      display.setTextColor(GxEPD_BLACK);
-    }
-
-      delay(500); //0.5sec
     }
   }
   //vTaskDelay(30000); //30sec
@@ -192,31 +230,23 @@ void get_weather(){
 void setup()
 {
     
-    //Serial.begin(115200);
-    //Serial.println();
-    //Serial.println("setup");
+    /*     Serial.begin(115200);
+    Serial.println();
+    Serial.println("setup"); */
+
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
     set_WiFi();
 
     SPI.begin(EPD_SCLK, EPD_MISO, EPD_MOSI);
 
-    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
     ++bootCount;
-
-    delay(500);
-   
-
+  
 }
-
-//String array[] = {'Snow', 'Thunderstorm', 'Drizzle', 'Rain', 'Clear', 'Clouds'};
 
 void loop()
 {
     
-    display.init();
-
-    display.fillScreen(GxEPD_WHITE);
-
     get_weather();
 
     display.setRotation(1);
@@ -292,23 +322,23 @@ void loop()
     display.setFont(&FreeSansBold18pt7b);
 
     if((int)round(main_temp_03) < -9){
-      display.setCursor(186-83, 115);
+      display.setCursor(186-89, 115);
       display.print((int)round(main_temp_03));
-      display.setCursor(237-83, 95);
+      display.setCursor(237-89, 95);
     }else if((int)round(main_temp_03) >= 10 || (int)round(main_temp_03) < 0){
-      display.setCursor(187-83, 115);
+      display.setCursor(187-89, 115);
       display.print((int)round(main_temp_03));
-      display.setCursor(224-83, 95);
+      display.setCursor(224-89, 95);
     }else{
-      display.setCursor(200-83, 115);
+      display.setCursor(200-89, 115);
       display.print((int)round(main_temp_03));
-      display.setCursor(219-83, 95);
+      display.setCursor(219-89, 95);
     }
 
     display.setFont(&FreeSansBold9pt7b);
     display.print("o");
 
-    display.setCursor(148, 25);//63
+    display.setCursor(154, 25);//63
     display.print(houre_wifi_03);
 
     display.setFont(&FreeMonoBold9pt7b);
@@ -317,7 +347,7 @@ void loop()
     
     display.update();
 
-    delay(2000);
+    delay(100);
     esp_deep_sleep_start();
 
 }
